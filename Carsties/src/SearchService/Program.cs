a534a80be7f9e8040512ed378083db6cf9be1d0f
FIlipe Coelho <37293450+filipembcoelho@users.noreply.github.com>
2023-09-02 15:38:58 +0100

    Adding a more resilient HTTP communication | End of session 3

--- Carsties/src/SearchService/Program.cs
+++ Carsties/src/SearchService/Program.cs
@@ -1,5 +1,9 @@
+using System.Net;
+using Microsoft.VisualBasic;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -9,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionsHttpClient>();
+builder.Services.AddHttpClient<AuctionsHttpClient>().AddPolicyHandler(GetPolicy());
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -20,13 +24,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.Initdb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-}
+    try
+    {
+        await DbInitializer.Initdb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() =>
+    HttpPolicyExtensions.HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
